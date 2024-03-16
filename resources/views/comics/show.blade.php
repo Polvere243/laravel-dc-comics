790d1187ebe896368192fe0ca43241a0b7aa8d39
Polvere243 <danielercoli@gmail.com>
2024-03-16 21:32:51 +0100

    added links with icons

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,7 +2,6 @@
 
 @section('title', 'Fumetto')
 
-@section('cdns', '<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css' integrity='sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==' crossorigin='anonymous'/>')
 
 
 @section('main')
@@ -10,6 +9,14 @@
     <figure>
         <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
     </figure>
+    <div class="icons">
+        <a href="">
+            <i class="fas fa-pencil"></i>
+        </a>
+        <a href="">
+            <i class="far fa-plus"></i>
+        </a>
+    </div>
 </div>
 <section id="comic-detail">
     <div class="detail-card">
