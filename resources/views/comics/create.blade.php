143e3cbba181d25873d873aed1c90d33211875a9
Polvere243 <danielercoli@gmail.com>
2024-03-17 03:31:38 +0100

    started working on the form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -4,6 +4,12 @@
 
 @section('main')
 
+<div class="container">
+    <h1>Crea un fumetto</h1>
+    <a href="{{ route('comics.show')}}">Indietro</a>
+    <form action="" method="POST">
 
+    </form>
+</div>
 
 @endsection
